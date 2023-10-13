dd40e09dd3a9b06fa88c5762a02d01ea4af767c4
gerbsec <gerbsec@proton.me>
2023-10-12 20:59:22 -0400

    asdf

--- Havoc/Havoc-Install.sh
+++ Havoc/Havoc-Install.sh
@@ -5,6 +5,7 @@ wget https://go.dev$go
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $(echo $go | awk -F "/" '{print $3}')
 rm -rf $(echo $go | awk -F "/" '{print $3}')
 mkdir -p ~/go
+echo -e "export PATH=~/.local/bin:$PATH\nexport PATH=$PATH:/usr/local/go/bin\nexport GOPATH=$HOME/go\nexport PATH=$PATH:$GOPATH/bin\n" > ~/.zshrc
 
 
 git clone https://github.com/HavocFramework/Havoc.git
@@ -23,4 +24,3 @@ make client-build
 
 # Run the teamserver
 echo "./havoc server --profile ../profiles/office365.yaotl -v --debug"
-echo -e "export PATH=~/.local/bin:$PATH\nexport PATH=$PATH:/usr/local/go/bin\nexport GOPATH=$HOME/go\nexport PATH=$PATH:$GOPATH/bin\n" > ~/.zshrc
